59c6c4f07a84bd1467f73d350894d9285a3b5601
mebry <vadushko@gmail.com>
2023-03-31 07:05:15 +0300

    update the main files. Add routing and stylization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Posts from './components/Posts';
+import Header from './components/Header';
+import Rating from './components/Rating';
+import Tags from './components/Tags';
 
 function App() {
+  let tagList=[
+    { title: 'Google', href: 'https://www.google.com/'},
+    { title: 'Метанит', href: 'https://metanit.com/web/javascript/'},
+    { title: 'Справочник по реакту', href: 'https://ru.reactjs.org/'},
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header logoURL={logo}
+        title="Приложение на реакате" />
+      <Navbar />
+      <Routes>
+        <Route path="stars" element={<Rating maxStars={5} selectedStars={1} />}></Route>
+        <Route path="tags" element={<Tags tags={tagList} />}></Route>
+        <Route path="pagination" element={<Posts count={100} limit={10} />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
